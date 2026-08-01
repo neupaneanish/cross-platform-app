aaa10b3a65b0e86e5bbd181449dcb4137a236337
Anish Neupane <anishneupane1@gmail.com>
2026-08-01 00:52:43 -0700

    ForgetPassword Service

--- App.axaml.cs
+++ App.axaml.cs
@@ -52,6 +52,7 @@ public class App : Application
         // Services
         serviceCollection.AddSingleton<ITokenService, TokenService>();
         serviceCollection.AddTransient<LoginService>();
+        serviceCollection.AddTransient<ForgetPasswordService>();
         serviceCollection.AddTransient<RegisterService>();
 
         // ViewModels
---
+++ Services/ForgetPasswordService.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using TuinFounder.External.Authentication.V1;
+
+namespace TuinFounder.Services;
+
+public class ForgetPasswordService(ExternalAuthenticationService.ExternalAuthenticationServiceClient client)
+{
+    public async Task<ForgetPasswordResponse> ForgetPasswordAsync(
+        string email,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var request = new ForgetPasswordRequest { Email = email };
+
+        return await client.ForgetPasswordAsync(request, cancellationToken: cancellationToken);
+    }
+}
