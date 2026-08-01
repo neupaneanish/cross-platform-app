3ceef1d9e5b3abe3c0c18805d0c06be2c5c07d88
Anish Neupane <anishneupane1@gmail.com>
2026-07-31 23:23:25 -0700

    Add gRPC Client

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,13 +1,19 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
+using TuinFounder.External.Authentication.V1;
+using TuinFounder.Gateway.Authentication.V1;
 using TuinFounder.ViewModels;
 using TuinFounder.Views;
 
 namespace TuinFounder;
 
-public partial class App : Application
+public class App : Application
 {
+    private ServiceProvider? _serviceProvider;
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -15,14 +21,41 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        var serviceCollection = new ServiceCollection();
+        RegisterServices(serviceCollection);
+        _serviceProvider = serviceCollection.BuildServiceProvider(new ServiceProviderOptions
+        {
+            ValidateOnBuild = true,
+            ValidateScopes = true
+        });
+
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainView
-            {
-                DataContext = new MainViewModel(),
-            };
+            desktop.MainWindow = _serviceProvider.GetRequiredService<MainView>();
+            desktop.ShutdownRequested += (_, _) => _serviceProvider.Dispose();
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private static void RegisterServices(IServiceCollection serviceCollection)
+    {
+        var url = new Uri("https://founder.tuin.dev");
+
+        // gRPC Clients
+        serviceCollection.AddGrpcClient<ExternalAuthenticationService.ExternalAuthenticationServiceClient>(client =>
+            client.Address = url);
+        serviceCollection.AddGrpcClient<GatewayAuthenticationService.GatewayAuthenticationServiceClient>(client =>
+            client.Address = url);
+
+        // ViewModels
+        serviceCollection.AddTransient<MainViewModel>();
+
+        // Views
+        serviceCollection.AddTransient<MainView>(sp =>
+        {
+            var viewModel = sp.GetRequiredService<MainViewModel>();
+            return new MainView { DataContext = viewModel };
+        });
     }
 }
