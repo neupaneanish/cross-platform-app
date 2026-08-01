553d94507bf5ca677f1ac525ed95e37dfcbeee48
Anish Neupane <anishneupane1@gmail.com>
2026-07-31 23:04:28 -0700

    Initial Commit

---
+++ App.axaml.cs
@@ -0,0 +1,28 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using TuinFounder.ViewModels;
+using TuinFounder.Views;
+
+namespace TuinFounder;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new MainView
+            {
+                DataContext = new MainViewModel(),
+            };
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
---
+++ Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TuinFounder.Views;
+
+public partial class MainView : Window
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
